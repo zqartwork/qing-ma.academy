b00834700b3bb7e69e4464f60acddcd8a96cb68d
zqartwork <zqartworkofficial@gmail.com>
2019-03-18 17:42:15 +0800

    PlayList 樣板完成

--- js/custom.js
+++ js/custom.js
@@ -272,3 +272,17 @@ $(".read-more .button").click(function () {
     return false;
 
 });
+
+$(function () {
+    $("#playlist li").on("click", function () {
+        $("#videoarea").attr({
+            "src": $(this).attr("movieurl"),
+            "poster": "",
+            "autoplay": "autoplay"
+        })
+    })
+    $("#videoarea").attr({
+        "src": $("#playlist li").eq(0).attr("movieurl"),
+        "poster": $("#playlist li").eq(0).attr("moviesposter")
+    })
+})
