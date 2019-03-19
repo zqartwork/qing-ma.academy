959d92a674093eab66952c3fd7d03a1c3666c022
zqartwork <zqartworkofficial@gmail.comii>
2019-03-19 19:40:03 +0800

    Revert "作業區 2019-3-19 下班前"
    This reverts commit 449ab220c78d41ccd1676ff830740bd8eee49dcb.

--- js/custom.js
+++ js/custom.js
@@ -309,25 +309,3 @@ $('#exitbtn').click(function () {
             opacity: 1
         });
 })
-
-var inputs = document.querySelectorAll('.inputfile');
-Array.prototype.forEach.call(inputs, function (input) {
-    var label = input.nextElementSibling,
-        labelVal = label.innerHTML;
-
-    input.addEventListener('change', function (e) {
-        var fileName = '';
-        if (this.files && this.files.length > 1)
-            fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
-        else
-            fileName = e.target.value.split('\\').pop();
-
-        if (fileName)
-            label.querySelector('span').innerHTML = fileName;
-        else
-            label.innerHTML = labelVal;
-    });
-});
-
-input.addEventListener('focus', function () { input.classList.add('has-focus'); });
-input.addEventListener('blur', function () { input.classList.remove('has-focus'); });
