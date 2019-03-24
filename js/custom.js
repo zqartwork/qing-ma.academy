b12fd180805437fb91a21dc4defd6d89d1bc932b
zqartwork <zqartworkofficial@gmail.com>
2019-03-25 00:21:39 +0800

    課程按鈕的統整

--- js/custom.js
+++ js/custom.js
@@ -378,3 +378,19 @@ $('#slide-btn').click(function () {
 $('.list-item.ls').click(function () {
     $(this).toggleClass('active');
 });
+
+function activateSearchbox(el) {
+    el.classList.add('searchbox--active')
+}
+
+function deactivateSearchbox(el) {
+    el.classList.remove('searchbox--active')
+}
+
+function onFocus() {
+    activateSearchbox(document.querySelector('.searchbox'));
+}
+
+function onBlur() {
+    deactivateSearchbox(document.querySelector('.searchbox'));
+}
